a6186f967e8a01a3cc3685d409c3edd5ac4481d1
Satoru Sukawa <sukawasatoru.github@outlook.jp>
2023-02-05 15:31:50 +0900

    add base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // for GitHub Pages.
+  // base: '/darkmode-mui/',
   plugins: [react()],
 })
