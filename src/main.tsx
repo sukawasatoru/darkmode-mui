fe3150e7d31d25925f5d4d5e321737f72e33d7ab
Satoru Sukawa <sukawasatoru.github@outlook.jp>
2023-02-07 00:33:51 +0900

    Remove unneeded server-side code

--- src/main.tsx
+++ src/main.tsx
@@ -25,7 +25,7 @@ import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <CssVarsProvider>
+    <CssVarsProvider defaultMode='system' modeStorageKey='appearance'>
       <CssBaseline/>
       <App/>
     </CssVarsProvider>
